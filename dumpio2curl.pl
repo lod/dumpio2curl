502ec25a907a224a0a86a35d5c895be7d19ac4a2
David Tulloh <git-david@tulloh.id.au>
2016-09-13 14:49:37 +1000

    Rewrote log line regex
    Extensive rewrite based on the C source code

--- dumpio2curl.pl
+++ dumpio2curl.pl
@@ -56,13 +56,84 @@ sub req_to_curl {
 	say "http://$partial_url" if $partial_url;
 }
 
+#[Wed Jan 07 10:50:05.743358 2015] [dumpio:trace7] [pid 21414] mod_dumpio.c(103): [client ::1:33975] mod_dumpio:  dumpio_in (data-HEAP): X-Forwarded-Server: localhost:88\r\n
+#
+# dumpio format, from mod_dumpio.c 2016-09-13
+#	All lines have the standard apache2 format (unless overridden in the server config):
+#		[<Date string>] [<log level>] [pid <pid number>] <filename>(<line number>): [client <details>] <specific output>\r\n
+#	There are multiple output lines by dumpio, each with their own format
+#	I am identifying them here by linenumber but not when matching, they are too fragile
+#
+#	dumpit() #58    "mod_dumpio:  %s (%s-%s): %d bytes"
+#		1: filter frec name ("dumpio_in" or "dumpio_out")
+#		2: "metadata" or "data"
+#		3: bucket type name (lots, we shouldn't care... HEAP, TRANSIENT, EOS, FLUSH etc.)
+#		4: bucket length (in APR_SIZE_T_FMT, %I64d for Windows, %d otherwise)
+#
+#	dumpit() #87 & #100    "mod_dumpio:  %s (%s-%s): %s"
+#		1: filter frec name
+#		2: "metadata" or "data"
+#		3: bucket type name
+#		4: bucket contents
+#		Only output if it is not a metadata bucket
+#		Bucket contents may be broken across multiple messages if required
+#		Two forms dealing with different charsets, comments indicate that there are still issues
+#
+#	dumpit() #109    "mod_dumpio:  %s (%s-%s): %s"
+#		1: filer frec name
+#		2: "metadata" or "data"
+#		3: bucket type name
+#		4: "error reading data"
+#		Occurs if the data bucket read fails
+#
+#   dumpio_input_filter() #135    "mod_dumpio: %s [%s-%s] %d readbytes"
+#   	1: filter frec name
+#   	2: input mode qw/readbytes getline eatcrlf speculative exhausitve init unknown/
+#   	3: read type "blocking" or "nonblocking"
+#   	4: number of readbytes
+#
+#   dumpio_input_filter() #146
+#   	Calls dumpit() for each bucket in the bucket brigade
+#
+#   dumpio_input_filter() #150    "mod_dumpio: %s - %d"
+#   	1: filter frec name
+#   	2: return code from failing ap_get_brigade()
+#   	Occurs if the bucket brigate get fails
+#
+#   dumpio_output_filter() #164    "mod_dumpio: %s"
+#   	1: filter frec name
+#
+#   dumpio_output_filter() #174
+#   	Calls dumpit() for each bucket in the bucket brigade
 
 while(<>) {
-	#[Wed Jan 07 10:50:05.743358 2015] [dumpio:trace7] [pid 21414] mod_dumpio.c(103): [client ::1:33975] mod_dumpio:  dumpio_in (data-HEAP): X-Forwarded-Server: localhost:88\r\n
-	my ($timestamp, $pid, $client, $direction, undef, $data) = /^\[([^\]]*)\]\s+\[dumpio:trace7\]\s+\[pid\s+(\d+)\].*\[client\s+([^\]]+)\]\s+mod_dumpio:\s+dumpio_(\w+)\s+\(data-(HEAP|TRANSIENT)\):\s+(.*)$/x;
-
+	# Peal off the Apache log format
+	# This can be found in the source code /server/log.c do_errorlog_default()
+	# I believe it can be changed via config, but if you break it you can fix it
+	my ($timestamp, $pid_str, $client, $log_msg) = /^
+		\[([^\]]*)\] \s+    (?# Date in ctime format )
+		\[dumpio:\w+\] \s+  (?# Module name : log level - typically trace7 )
+		\[([^\]]+)\]        (?# process and thread id if applicable "pid ###" or "pid ###:tid ###" )
+		                    (?# If log level >= debug, the filename and line number are here )
+		                    (?# If status is set it is output here )
+		.*                  (?# Discard the above two possible bits of information)
+		\[client\s+([^\]]+)\] \s+ (?# Client identification details, ip:port )
+		                    (?# Above client may be "remote", we don't want them )
+		(.*?)               (?# The actual specific log message)
+		\r?\n$
+	/x;
 	next unless defined($timestamp);
-	next if $data =~ /^(\d+)\sbytes$/; # Just length, not content
+
+	# We only want the data packets, defined above as dumpit() #87 & #100
+	# "mod_dumpio:  %s (%s-%s): %s"
+	my ($direction, $data) = $log_msg =~ /^mod_dumpio: \s+ dumpio_(\w+) \s+ \(data-\w+\): \s+ (.*)$/x;
+
+	next unless defined($direction); # Didn't match
+	next unless $direction eq "out" or $direction eq "in"; # Shouldn't happen, but better safe
+
+	# Ditch the very similar looking #58 and #109 outputs
+	next if $data =~ /^(\d+)\sbytes$/;     # #58 message, no data
+	next if $data eq "error reading data"; # #109 message, error. Silently ignoring it can't go wrong
 
 	# Content ends with "/r/n" - strip it off
 	$data =~ s/\\r\\n$//;
@@ -85,7 +156,7 @@ while(<>) {
 		}
 
 		# Print header at start of block - ensures right metadata is used
-		say "# $timestamp - pid $pid - client $client";
+		say "# $timestamp - $pid_str - client $client";
 	}
 
 	if ($direction eq "out") { # Server output
@@ -154,7 +225,7 @@ David Tulloh <dumpio-david@tulloh.id.au>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2015 David Tulloh
+Copyright 2015-2016 David Tulloh
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
